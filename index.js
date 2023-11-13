d8687c8da67dede64be45868c54f804a5c69247b
Vitorv7 <viitor.dasilvaa@gmail.com>
2023-11-13 15:04:03 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+window.sr = ScrollReveal({ reset: true })
+
+/*TRADIÇÃO*/
+ScrollReveal().reveal('#visual-main', {
+  duration: 2000
+})
+
+/*COLUNAS-HORIZONTAL*/
+ScrollReveal().reveal('#colum', {
+  duration: 2000
+})
+
+/*EXPERIÊNCIA*/
+ScrollReveal().reveal('#content-home', {
+  duration: 2000
+})
+
+/*FORMULARIO*/
+ScrollReveal().reveal('#form', {
+  rotate: { x: 80, y: 0, z: 0 },
+  duration: 2000
+})
